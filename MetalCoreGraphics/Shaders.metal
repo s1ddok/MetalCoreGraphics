bfcbab7bb61623c831da1a978369da8d0aac1737
s1ddok <siddok@gmail.com>
2019-03-05 00:14:12 +0300

    draw something with metal

---
+++ MetalCoreGraphics/Shaders.metal
@@ -0,0 +1,47 @@
+//
+//  Shaders.metal
+//  MetalCoreGraphics
+//
+//  Created by Andrey Volodin on 04.03.2018.
+//  Copyright © 2019 Andrey Volodin. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+struct MTLTextureViewVertexOut {
+    float4 position [[ position ]];
+    float2 uv;
+};
+
+vertex MTLTextureViewVertexOut vertexFunc(uint vid [[vertex_id]]) {
+    MTLTextureViewVertexOut out;
+
+    const float2 vertices[] = { float2(-1.0f, 1.0f), float2(-1.0f, -1.0f),
+        float2(1.0f, 1.0f), float2(1.0f, -1.0f)
+    };
+
+    out.position = float4(vertices[vid], 0.0, 1.0);
+    float2 uv = vertices[vid];
+    uv.y = -uv.y;
+    out.uv = fma(uv, 0.5f, 0.5f);
+
+    return out;
+}
+
+fragment half4 fragmentFunc(MTLTextureViewVertexOut in [[stage_in]],
+                            texture2d<half, access::sample> original [[texture(0)]],
+                            texture2d<half, access::sample> blurred [[texture(1)]],
+                            texture2d<half, access::sample> visibilityMask [[texture(2)]])
+{
+    constexpr sampler s(coord::normalized,
+                        address::clamp_to_zero,
+                        filter::linear);
+
+    half4 originalColor = original.sample(s, in.uv);
+    half4 blurredColor = blurred.sample(s, in.uv);
+    half mask = visibilityMask.sample(s, in.uv).r;
+
+    return blurredColor;//mix(originalColor, blurredColor, mask);
+}
+
--- MetalCoreGraphics/ViewController.swift
+++ MetalCoreGraphics/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Alloy
+import MetalKit
+import MetalPerformanceShaders
 
 // Returns a size of the 'inSize' aligned to 'align' as long as align is a power of 2
 func alignUp(size: Int, align: Int) -> Int {
@@ -21,12 +23,50 @@ func alignUp(size: Int, align: Int) -> Int {
 }
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var metalView: MTKView!
+
+
+    let metalContext = MTLContext(device: Metal.device)
+
+    var originalTexture: MTLTexture?
+    var blurredTexture: MTLTexture?
+    var renderState: MTLRenderPipelineState?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let puppyImage = UIImage(named: "puppy")!
+
+        self.originalTexture = try! self.metalContext.texture(from: puppyImage.cgImage!)
+        self.blurredTexture = self.originalTexture?.matchingTexture(usage: [.shaderRead, .shaderWrite])
+
+        let blurShader = MPSImageGaussianBlur(device: self.metalContext.device,
+                                              sigma: 24.0)
+
+        try! self.metalContext.scheduleAndWait { buffer in
+            blurShader.encode(commandBuffer: buffer,
+                              sourceTexture: self.originalTexture!,
+                              destinationTexture: self.blurredTexture!)
+        }
+
+        let defaultLibrary = self.metalContext.standardLibrary!
+        let fragment = defaultLibrary.makeFunction(name: "fragmentFunc")
+        let vertex = defaultLibrary.makeFunction(name: "vertexFunc")
+
+        let descriptor = MTLRenderPipelineDescriptor()
+        descriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        descriptor.vertexFunction = vertex
+        descriptor.fragmentFunction = fragment
+
+        self.renderState = try! self.metalContext
+                                    .device
+                                    .makeRenderPipelineState(descriptor: descriptor)
+
+        self.metalView.depthStencilPixelFormat = .invalid
+        self.metalView.device = self.metalContext.device
+        self.metalView.delegate = self
+
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -54,14 +94,6 @@ class ViewController: UIViewController {
         context?.addLine(to: CGPoint(x: 512, y: 512))
         context?.strokePath()
 
-
-        let image = context!.makeImage()!
-
-        let uiimage = UIImage(cgImage: image)
-
-        self.imageView.image = uiimage
-
-        let metalContext = MTLContext(device: Metal.device)
         let buffer = metalContext.device.makeBuffer(bytesNoCopy: context!.data!,
                                                     length: context!.bytesPerRow * context!.height,
                                                     options: .storageModeShared,
@@ -77,9 +109,23 @@ class ViewController: UIViewController {
         let texture = buffer!.makeTexture(descriptor: textureDescriptor,
                                           offset: 0,
                                           bytesPerRow: context!.bytesPerRow)
-
     }
-
 
 }
 
+extension ViewController: MTKViewDelegate {
+    func draw(in view: MTKView) {
+        try? self.metalContext.scheduleAndWait { buffer in
+            buffer.render(descriptor: view.currentRenderPassDescriptor!) { encoder in
+                encoder.setRenderPipelineState(self.renderState!)
+                encoder.set(fragmentTextures: [self.originalTexture, self.blurredTexture, nil])
+                encoder.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
+            }
+
+            buffer.present(view.currentDrawable!)
+        }
+    }
+
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+    }
+}
