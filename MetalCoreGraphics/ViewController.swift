4d7488c1344be3956857e703e546a76b9c3fbb4d
s1ddok <siddok@gmail.com>
2019-03-04 23:24:36 +0300

    initial tests that cgcontext has something meaningful

--- MetalCoreGraphics/ViewController.swift
+++ MetalCoreGraphics/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let context = CGContext(data: nil,
+                                width: 512,
+                                height: 512,
+                                bitsPerComponent: 8,
+                                bytesPerRow: 0,
+                                space: CGColorSpaceCreateDeviceGray(),
+                                bitmapInfo: CGImageAlphaInfo.none.rawValue)
+
+        context?.setLineWidth(10.0)
+        context?.setStrokeColor(gray: 1.0, alpha: 1.0)
+        context?.beginPath()
+        context?.move(to: .zero)
+        context?.addLine(to: CGPoint(x: 512, y: 512))
+        context?.strokePath()
+
+
+        let image = context!.makeImage()!
+
+        let uiimage = UIImage(cgImage: image)
+
+        self.imageView.image = uiimage
+
+        
     }
 
 
